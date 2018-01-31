ad521f16d154a68e574e2a02df2ed21234820eda
warren <warren_as123@hotmail.com>
2018-01-31 12:15:46 -0500

    maste

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const  app=require('./app');
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-
+console.log("ss");
 app.listen(port,()=>{
     console.log("servidoer creado");
 });
